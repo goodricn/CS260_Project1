a9bee2b6e56f03257168d5da830016beaa6731c7
Nicholas Goodrich <ngoodrichl33t@gmail.com>
2014-01-22 16:00:15 -0500

    reFactor

---
+++ src/main/java/edu/union/adt/graph/HashGraph.java
@@ -0,0 +1,239 @@
+package edu.union.adt.graph;
+import java.util.*;
+
+/**
+ * A graph that establishes connections (edges) between objects of
+ * (parameterized) type V (vertices).  The edges are directed.  An
+ * undirected edge between u and v can be simulated by two edges: (u,
+ * v) and (v, u).
+ *
+ * The API is based on one from
+ *     http://introcs.cs.princeton.edu/java/home/
+ *
+ * Some method names have been changed, and the Graph type is
+ * parameterized with a vertex type V instead of assuming String
+ * vertices.
+ *
+ * @author Aaron G. Cass
+ * @version 1
+ */
+public class Graph<V>
+{
+
+    private Map < V, List<V>> edges;
+
+    /**
+     * Create an empty graph.
+     */
+    public Graph() 
+    {
+         edges = new HashMap <V, List<V>>();
+    }
+
+    /**
+     * @return the number of vertices in the graph.
+     */
+    public int numVertices()
+    {
+        return edges.size();
+    }
+
+    /**
+     * @return the number of edges in the graph.
+     */
+    public int numEdges()
+    {
+        //Iterator edgesList edges.values().iterator();
+        if(edges.isEmpty()){
+            return 0;
+        }
+        else{
+        boolean exit = false;
+        Iterator vertices = edges.keySet().iterator();
+        V nextKey;
+        int count = 0;
+        while(!exit){
+            nextKey = (V)vertices.next();
+            count += degree(nextKey); 
+            if(vertices.hasNext() == false){exit = true;}
+        }
+        return count;}
+    }
+    
+
+    /**
+     * Gets the number of vertices connected by edges from a given
+     * vertex.  If the given vertex is not in the graph, throws a
+     * RuntimeException.
+     *
+     * @param vertex the vertex whose degree we want.
+     * @return the degree of vertex 'vertex'
+     */
+    public int degree(V vertex)
+    {
+        if(edges.isEmpty()){
+            throw new RuntimeException("originalException");
+        }
+        else{
+        return edges.get(vertex).size();
+    }
+    }
+    
+
+    /**
+     * Adds a directed edge between two vertices.  If there is already an edge
+     * between the given vertices, does nothing.  If either (or both)
+     * of the given vertices does not exist, it is added to the
+     * graph before the edge is created between them.
+     *
+     * @param from the source vertex for the added edge
+     * @param to the destination vertex for the added edge
+     */
+    public void addEdge(V from, V to)
+    {
+        if(!edges.containsKey(from)){
+            addVertex(from);
+        }
+        if(!edges.containsKey(to)){
+            addVertex(to);
+        }
+        if(!edges.get(from).contains(to)){
+        edges.get(from).add(to);
+        }
+        
+
+        }
+
+    /**
+     * Adds a vertex to the graph.  If the vertex already exists in
+     * the graph, does nothing.  If the vertex does not exist, it is
+     * added to the graph, with no edges connected to it.
+     *
+     * @param vertex the vertex to add
+     */
+    public void addVertex(V vertex)
+    {
+       if(edges.containsKey(vertex)) return;
+        else
+        {
+        edges.put(vertex, new ArrayList<V>());
+       }
+    }
+
+    /**
+     * @return the an iterable collection for the set of vertices of
+     * the graph.
+     */
+    public Iterable<V> getVertices()
+    {
+        return edges.keySet();
+    }
+
+    /**
+     * Gets the vertices adjacent to a given vertex.  A vertex y is
+     * "adjacent to" vertex x if there is an edge (x, y) in the graph.
+     * Because edges are directed, if (x, y) is an edge but (y, x) is
+     * not an edge, we would say that y is adjacent to x but that x is
+     * NOT adjacent to y.
+     *
+     * @param from the source vertex
+     * @return an iterable collection for the set of vertices that are
+     * the destinations of edges for which 'from' is the source
+     * vertex.  If 'from' is not a vertex in the graph, returns an
+     * empty iterator.
+     */
+    public Iterable<V> adjacentTo(V from)
+    {   
+        return edges.get(from);
+    }
+
+    /**
+     * Tells whether or not a vertex is in the graph.
+     *
+     * @param vertex a vertex
+     * @return true iff 'vertex' is a vertex in the graph.
+     */
+    public boolean contains(V vertex)
+    {
+        return edges.containsKey(vertex);
+    }
+
+    /**
+     * Tells whether an edge exists in the graph.
+     *
+     * @param from the source vertex
+     * @param to the destination vertex
+     *
+     * @return true iff there is an edge from the source vertex to the
+     * destination vertex in the graph.  If either of the given
+     * vertices are not vertices in the graph, then there is no edge
+     * between them.
+     */
+    public boolean hasEdge(V from, V to)
+    {
+        return edges.containsKey(from) && edges.get(from).contains(to);
+    }
+
+
+    /** 
+    *@param a graph
+    *A graph is equal to another graph if and only if they contain the same verticies
+    *as well as the same edges
+    *@return returns true if and only if the two graphs are the same
+    */
+    public boolean equals(Object graphObject){
+
+       Graph graphTest = (Graph)graphObject;
+       return this.edges.equals(graphTest.edges);
+
+
+    }
+
+    /**
+     * Gives a string representation of the graph.  The representation
+     * is a series of lines, one for each vertex in the graph.  On
+     * each line, the vertex is shown followed by ": " and then
+     * followed by a list of the vertices adjacent to that vertex.  In
+     * this list of vertices, the vertices are separated by ", ".  For
+     * example, for a graph with String vertices "A", "B", and "C", we
+     * might have the following string representation:
+     *
+     * <PRE>
+     * A: A, B
+     * B:
+     * C: A, B
+     * </PRE>
+     *
+     * This representation would indicate that the following edges are
+     * in the graph: (A, A), (A, B), (C, A), (C, B) and that B has no
+     * adjacent vertices.
+     *
+     * Note: there are no extraneous spaces in the output.  So, if we
+     * replace each space with '*', the above representation would be:
+     *
+     * <PRE>
+     * A:*A,*B
+     * B:
+     * C:*A,*B
+     * </PRE>
+     *
+     * @return the string representation of the graph
+     */
+    public String toString()
+    {
+        
+        String output = "";
+
+        for(V v: edges.keySet()){
+            for(V k: edges.get(v)){
+            output += v + ":" + edges.get(k)+ "," + "\n";
+         }
+        }
+        return output;
+
+        
+        
+    
+    }
+
+}
