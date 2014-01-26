66659345dd38d2782daa91ed0bbda0dd48227786
Nicholas Goodrich <ngoodrichl33t@gmail.com>
2014-01-26 18:49:04 -0500

    basicTestsCompleted

--- src/test/java/edu/union/adt/graph/tests/GraphTestSuite.java
+++ src/test/java/edu/union/adt/graph/tests/GraphTestSuite.java
@@ -3,6 +3,7 @@ package edu.union.adt.graph.tests;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import edu.union.adt.graph.tests.GoodrichGraphTests;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses
---
+++ src/test/java/edu/union/adt/graph/tests/goodricn/GoodrichGraphTests.java
@@ -0,0 +1,90 @@
+package edu.union.adt.graph.tests.goodricn;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import edu.union.adt.graph.Graph;
+import edu.union.adt.graph.GraphFactory;
+
+@RunWith(JUnit4.class)
+public class GoodrichGraphTests
+{
+	private Graph<String> g;
+
+    @Before
+    public void setUp()
+    {
+        g = GraphFactory.<String>createGraph();
+    }
+
+    @After
+    public void tearDown()
+    {
+        g = null;
+    }
+    
+    @Test
+    public void isEmpty()
+    {
+    	assertTrue("Empty graph returns not empty",g.isEmpty());
+    	g.addVertex("foo");
+    	assertFalse("not empty graph returns empty",g.isEmpty());
+        g.removeVertex("foo");
+        assertTrue("Empty graph returns not empty after removal",g.isEmpty());
+    }
+
+    @Test 
+    public void removeVertex()
+    {
+        g.addVertex("foo");
+        assertFalse("The added vertex was not removed", g.contains("foo"));
+    }
+
+    @Test
+    public void removeEdge(){
+        g.addEdge("foo","bar");
+        g.removeEdge("foo","bar");
+        assertFalse("removed edge is still there", g.hasEdge("foo","bar"));
+    }
+
+    @Test
+    public void hasPath()
+    {
+        g.addEdge("foo","bar");
+        g.addEdge("bar","end");
+        assertTrue("there is no path from initial to final", g.hasPath("foo","end"));
+    }
+
+    @Test
+    public void pathLength()
+    {
+        g.addEdge("foo","bar");
+        g.addEdge("bar","end");
+        assertEquals("path length of one is incorrect", g.pathLength("foo","bar"),1);
+        assertEquals("path length larger than one is incorrect", g.pathLength("foo","end"),2);
+    }
+
+    @Test public void getPath()
+    {
+        g.addEdge("foo","bar");
+        g.addEdge("bar","end");
+        Iterable pathIterable = g.getPath("foo","end");
+        String[] correctPath = new String[g.numVertices()];
+        assertEquals("paths are not the same", pathIterable,correctPath);
+
+
+    }
+
+
+
+
+}
