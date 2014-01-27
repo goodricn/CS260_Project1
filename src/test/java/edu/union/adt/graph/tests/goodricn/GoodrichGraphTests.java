932d2398ffccd1ea979c750c073a1957e75a4f4a
Nicholas Goodrich <ngoodrichl33t@gmail.com>
2014-01-26 19:05:36 -0500

    simpleTestsNewAPI

--- src/test/java/edu/union/adt/graph/tests/goodricn/GoodrichGraphTests.java
+++ src/test/java/edu/union/adt/graph/tests/goodricn/GoodrichGraphTests.java
@@ -14,6 +14,7 @@ import org.junit.runners.JUnit4;
 
 import edu.union.adt.graph.Graph;
 import edu.union.adt.graph.GraphFactory;
+import java.util.*;
 
 @RunWith(JUnit4.class)
 public class GoodrichGraphTests
